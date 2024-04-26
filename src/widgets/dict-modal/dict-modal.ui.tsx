5d303b134c207cf6dd0ca3425c2e1bfb2914ab68
GrafDev <greg.yakovlev@hotmail.com>
2024-04-26 12:20:00 +0200

    Make form. TODO:Form of Dict

--- src/widgets/dict-modal/dict-modal.ui.tsx
+++ src/widgets/dict-modal/dict-modal.ui.tsx
@@ -10,13 +10,19 @@ import {
 import {FormDict} from "./form-dict.tsx";
 import {useDict, useDictModal} from "../../shared/store/zustand";
 import {IDictionaryItem} from "../../shared/types.ts";
-import {Form} from "react-router-dom";
+import {useForm} from "react-hook-form";
+import {Text} from "@chakra-ui/react";
 
 export const DictModal = ({isOpen, onClose}: { isOpen: boolean, onClose: () => void }) => {
     const editWord: IDictionaryItem = useDictModal((state) => state.editWord)
     const indexEditWord: number = useDictModal((state) => state.indexEditWord)
     const setWordToCurrentDict = useDict((state) => state.setWordToCurrentDict)
     const addWordToCurrentDict = useDict((state) => state.addWordToCurrentDict)
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm<IDictionaryItem>()
 
     const handler = (flag: boolean) => {
         if (flag) {
@@ -37,29 +43,48 @@ export const DictModal = ({isOpen, onClose}: { isOpen: boolean, onClose: () => v
             isCentered>
             <ModalOverlay/>
             <ModalContent>
-                <Form>
-                <ModalHeader>{
-                    indexEditWord < 0 ? "Add new word" :("Edit " + editWord.word )
-                }
+
+                <ModalHeader justifyItems={"space-between"}
+                             alignItems={"center"}
+                             display={"flex"}
+                             justifyContent={"space-between"}
+                             pl={5}>
+                    <Text as={"span"}
+                          fontSize={{base: "sx", sm: "sx", md: "md", lg: "md", xl: "large", "2xl": "x-large"}}
+                          fontWeight={"thin"}>
+                        {indexEditWord < 0
+                            ? "Add"
+                            : "Edit"}
+                    </Text>
+                    <Text>
+                        {editWord.word}
+                    </Text>
+                    <Text>
+                        <ModalCloseButton/>
+
+                    </Text>
                 </ModalHeader>
-                <ModalCloseButton/>
-                <ModalBody>
+
+                <form>
+
+
+                    <ModalBody>
                         <FormDict/>
-                </ModalBody>
+                    </ModalBody>
 
-                <ModalFooter>
-                    <Button variant='ghost' onClick={() => handler(true)}>
-                        {indexEditWord >= 0 ? "Save" : "Add"}
-                    </Button>
-                    <Button colorScheme='blue'
-                            mr={3}
-                            onClick={() => handler(false)}
-                            type={"submit"}>
-                        Cancel
-                    </Button>
+                    <ModalFooter>
+                        <Button variant='ghost' onClick={() => handler(true)}>
+                            {indexEditWord >= 0 ? "Save" : "Add"}
+                        </Button>
+                        <Button colorScheme='blue'
+                                mr={3}
+                                onClick={() => handler(false)}
+                                type={"submit"}>
+                            Cancel
+                        </Button>
 
-                </ModalFooter>
-                </Form>
+                    </ModalFooter>
+                </form>
 
             </ModalContent>
         </Modal>
--- src/widgets/dict-modal/form-dict.tsx
+++ src/widgets/dict-modal/form-dict.tsx
@@ -1,18 +1,45 @@
-import {useDictModal} from "../../shared/store/zustand";
-import {Input, InputGroup, InputLeftAddon, VStack} from "@chakra-ui/react";
-import {IDictionaryItem} from "../../shared/types.ts";
-export const FormDict = ()=>{
-        const editWord:IDictionaryItem=useDictModal((state)=>state.editWord)
-return (
-    <VStack>
-            {Object.entries(editWord).map(([key, value]: [string, string]) => (
-                <InputGroup>
-                        <InputLeftAddon>{key}</InputLeftAddon>
-                        <Input type="text"
-                               placeholder={key}
-                               value={value}
-                               size={{base: "sm", sm: "md", md: "lg", lg: "xl", xl: "2xl", "2xl": "3xl"}} />
-                </InputGroup>
-            ))}
-    </VStack>
-)}
+import { useDictModal } from "../../shared/store/zustand";
+import { Input, InputGroup, InputLeftAddon, VStack } from "@chakra-ui/react";
+import { IDictionaryItem } from "../../shared/types.ts";
+
+export const FormDict = () => {
+    const editWord: IDictionaryItem = useDictModal((state) => state.editWord);
+    return (
+        <VStack>
+            {Object.entries(editWord).map(([key, value]: [string, any]) => {
+                if (
+                    key !== "id" &&
+                    key !== "word" &&
+                    key !== "popular" &&
+                    key !== "learning"
+                ) {
+                    return (
+                        <InputGroup
+                            key={key}
+
+                            size={{
+                                base: "sm",
+                                sm: "md",
+                                md: "lg",
+                                lg: "xl",
+                                xl: "2xl",
+                                "2xl": "3xl",
+                            }}
+                        >
+                            <InputLeftAddon>{key}</InputLeftAddon>
+                            <Input
+                                type="text"
+                                placeholder={key}
+                                value={value}
+                                onChange={(e) => {
+                                    // Handle changes here
+                                    editWord[key] = e.target.value;
+                                }}
+                            />
+                        </InputGroup>
+                    );
+                } else return null;
+            })}
+        </VStack>
+    );
+};
