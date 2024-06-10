f23332230a9c89e7d8f53fee45595a1c2b80f986
GrafDev <greg.yakovlev@hotmail.com>
2024-06-10 15:01:08 +0200

    Changes after move to other site name

--- src/shared/store/constants-store/constants-pictures.ts
+++ src/shared/store/constants-store/constants-pictures.ts
@@ -1,2 +1,2 @@
 
-export const HELP_ANIME="https://firebasestorage.googleapis.com/v0/b/benedict-24-05-11-17.appspot.com/o/Help%2FHelp.gif?alt=media&token=ca6046a4-2ad3-499d-8bd4-b2644ea51ec3"
+export const HELP_ANIME="https://firebasestorage.googleapis.com/v0/b/bendict.appspot.com/o/Help.gif?alt=media&token=c4c34e2e-b5b2-4a57-a14f-c8fceccd7922"
